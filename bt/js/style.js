4ffc7dddfecc6d1521e65b141ce1506150e5cb78
Your Name <you@example.com>
2019-08-08 14:44:47 +0800

    d

---
+++ bt/js/style.js
@@ -0,0 +1,26 @@
+$(function () {
+    var num = $(".ul1").find("li").length;
+    if (num > 1) {
+        setInterval(function () {
+            $('.ul1').animate({
+                marginTop: "-20px"
+            }, 500, function () {
+                $(this).css({ marginTop: "0" }).find("li:first").appendTo(this);
+            });
+        }, 3000);
+    }
+
+
+    $(".money_title li").on("click",function(){
+        var indx=$(this).index();
+        $(this).css({
+            "color":"#075bd9",
+            "border-bottom":"2px solid #075bd9",
+        }).siblings().css({
+            "color":"#333333",
+            "border-bottom":"2px solid transparent"
+        });
+        $(".money_content li").eq(indx).css("display","block").siblings().css("display","none");
+    })
+
+});
