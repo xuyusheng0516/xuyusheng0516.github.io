4303d93ad827435431a3468bb4947e3972bdac56
Your Name <you@example.com>
2019-08-09 15:21:45 +0800

    2019.8.9

--- bt/js/style.js
+++ bt/js/style.js
@@ -1,4 +1,5 @@
 $(function () {
+    // 公告
     var num = $(".ul1").find("li").length;
     if (num > 1) {
         setInterval(function () {
@@ -10,17 +11,20 @@ $(function () {
         }, 3000);
     }
 
-
-    $(".money_title li").on("click",function(){
-        var indx=$(this).index();
+    // 选项卡
+    $(".money_title li").on("click", function () {
+        var indx = $(this).index();
         $(this).css({
-            "color":"#075bd9",
-            "border-bottom":"2px solid #075bd9",
+            "color": "#075bd9",
+            "border-bottom": "2px solid #075bd9",
         }).siblings().css({
-            "color":"#333333",
-            "border-bottom":"2px solid transparent"
+            "color": "#333333",
+            "border-bottom": "2px solid transparent"
         });
-        $(".money_content li").eq(indx).css("display","block").siblings().css("display","none");
-    })
+        $(".money_content li").eq(indx).css("display", "block").siblings().css("display", "none");
+    });
+
+
+    
 
 });
