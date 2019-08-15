b6efd7e0204bb7b72783cb970bea4bf521eb3133
Your Name <you@example.com>
2019-08-15 14:35:38 +0800

    2019-08-15

--- bt/js/style.js
+++ bt/js/style.js
@@ -24,20 +24,52 @@ $(function () {
         $(".money_content li").eq(indx).css("display", "block").siblings().css("display", "none");
     });
     //点击 资金安全  专业风控  实力平台切换对应内容
-    $(".head_ul>li").on("click",function(){
-        var liIndex=$(this).index();
+    $(".head_ul>li").on("click", function () {
+        var liIndex = $(this).index();
         $(this).find("a").css({
-            "color":"#075bd9",
-            "background-color":"#fff"
+            "color": "#075bd9",
+            "background-color": "#fff"
         }).parent().siblings().find("a").css({
-            "color":"#fff",
-            "background-color":"#075bd9"
+            "color": "#fff",
+            "background-color": "#075bd9"
         });
-        $(".safe_money_content>div").eq(liIndex).css("display","block").siblings().css("display","none");
+        $(".safe_money_content>div").eq(liIndex).css("display", "block").siblings().css("display", "none");
+    });
+
+    // 支付密码
+    function getPaw() {
+        var $input = $(".fake-box input");
+        $("#pwd-input").on("input", function () {
+            var pwd = $(this).val().trim();
+            for (var i = 0, len = pwd.length; i < len; i++) {
+                $input.eq(i).val(pwd[i]);
+            }
+            $input.each(function () {
+                var index = $(this).index();
+                if (index >= len) {
+                    $(this).val("");
+                }
+            });
+            if (len == 6) {
+                // setTimeout(function () {
+                //     $(".loading_box").css("display", "block");
+                // }, 400);
+            }
+        });
+    }
+    getPaw();
+
+    $(".pay_money").on("click", function () {
+        $(".paying_box,.mask_container").css("display", "block");
+
+    });
+    $(".paying_box").on("click", function () {
+        $(".paying_box,.mask_container").css("display", "none");
+
     })
 
 
 
-    
+
 
 });
