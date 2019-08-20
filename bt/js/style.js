30881b11d3ca52d9d25dc98f50341df9fbdc71c6
Your Name <you@example.com>
2019-08-20 16:27:37 +0800

    19-8-20

--- bt/js/style.js
+++ bt/js/style.js
@@ -106,31 +106,91 @@ $(function () {
     });
 
     // 身份认证  点击上传
-    $(".click_zheng1").on("click",function(){
+    $(".click_zheng1").on("click", function () {
         $(".file_zheng1").click();
     });
-    $(".click_fang1").on("click",function(){
+    $(".click_fang1").on("click", function () {
         $(".file_fang1").click();
     });
-    $(".click_zheng2").on("click",function(){
+    $(".click_zheng2").on("click", function () {
         $(".file_zheng2").click();
     });
-    $(".click_fang2").on("click",function(){
+    $(".click_fang2").on("click", function () {
         $(".file_fang2").click();
     });
 
-    $(".id_test_t>p").on("click",function(){
-        var areaIndex=$(this).index();
+    $(".id_test_t>p").on("click", function () {
+        var areaIndex = $(this).index();
         $(this).css({
-            "color":"#fff",
-            "background-color":"#075bd9"
+            "color": "#fff",
+            "background-color": "#075bd9"
         }).siblings().css({
-            "color":"#075bd9",
-            "background-color":"transparent"
+            "color": "#075bd9",
+            "background-color": "transparent"
         });
-        $(".id_testing_box>form").eq(areaIndex).css("display","block").siblings().css("display","none");
+        $(".id_testing_box>form").eq(areaIndex).css("display", "block").siblings().css("display", "none");
     });
-    
+
+    //中国大陆地区   表单验证
+    $(".China_btn").on("click", function () {
+        var name_input1 = $(".name_input1").val();
+        var num_input1 = $(".num_input1").val();
+        var file_zheng1 = $(".file_zheng1").val();
+        var file_fang1 = $(".file_fang1").val();
+        var card_t = /(^\d{15}$)|(^\d{18}$)|(^\d{17}(\d|X|x)$)/;
+        if (name_input1 == "") {
+            $('.test_mask').show().html("姓名不能为空");
+            setTimeout(function () {
+                $('.test_mask').hide();
+            }, 1200);
+            return false;
+        } else if (num_input1 == "" || (!card_t.test(num_input1))) {
+            $('.test_mask').show().html("请输入有效身份证号");
+            setTimeout(function () {
+                $('.test_mask').hide();
+            }, 1200);
+            return false;
+        } else if (file_zheng1 == "" || file_fang1 == "") {
+            $('.test_mask').show().html("请上传身份证照片");
+            setTimeout(function () {
+                $('.test_mask').hide();
+            }, 1200);
+            return false;
+        } else {
+            $(".China_btn").attr("type", "submit");
+        }
+    });
+    //其他国家及地区   表单验证
+    $(".country_btn").on("click", function () {
+        var name_input2 = $(".name_input2").val();
+        var num_input2 = $(".num_input2").val();
+        var file_zheng2 = $(".file_zheng2").val();
+        var file_fang2 = $(".file_fang2").val();
+        var card_t = /(^\d{15}$)|(^\d{18}$)|(^\d{17}(\d|X|x)$)/;
+        if (name_input2 == "") {
+            $('.test_mask').show().html("姓名不能为空");
+            setTimeout(function () {
+                $('.test_mask').hide();
+            }, 1200);
+            return false;
+        } else if (num_input2 == "" || (!card_t.test(num_input2))) {
+            $('.test_mask').show().html("请输入有效身份证号");
+            setTimeout(function () {
+                $('.test_mask').hide();
+            }, 1200);
+            return false;
+        } else if (file_zheng2 == "" || file_fang2 == "") {
+            $('.test_mask').show().html("请上传身份证照片");
+            setTimeout(function () {
+                $('.test_mask').hide();
+            }, 1200);
+            return false;
+        } else {
+            $(".country_btn").attr("type", "submit");
+        }
+    });
+
+
 
 
 
